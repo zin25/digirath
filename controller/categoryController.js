bd93d9f3fe1c93dd3e50e70682b6498c89d9b2b9
zin25 <jozzieazin@gmail.com>
2023-02-20 20:53:24 +0700

    Add Product Category

--- controller/categoryController.js
+++
@@ -1,3 +0,0 @@
-const Category = require("../models/categoryModel");
-const asyncHandler = require("express-async-handler");
-const validateMongoDbId = require("../utils/validateMongoDbId");
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 8000;
 const authUserRouter = require("./routes/authUserRoute");
 const productRouter = require("./routes/productRoute");
 const blogRouter = require("./routes/blogRoute");
+const prodCategoryRouter = require("./routes/prodCategoryRoute");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 dbConnect();
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use("/api/user", authUserRouter); // Base Url : http://localhost:8000/api/users/?
 app.use("/api/product", productRouter); // Base url : Product localhost:8000/api/product/?
 app.use("/api/blog", blogRouter); // Base url : Blog localhost:8000/api/blog/?
+app.use("/api/category", prodCategoryRouter); // Base url : Category localhost:8000/api/category/?
 
 // Middleware
 app.use(notFound);
